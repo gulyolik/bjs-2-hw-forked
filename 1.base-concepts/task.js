da4ef37a2271c198cbd96759f044d78e458df652
gulyolik <olia.guliaeva@yandex.ru>
2023-10-25 23:19:38 +0300

    modified task file

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,30 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+  let d = b ** 2 - 4 * a * c;
+	let x;
+	let y;
+	if (d < 0) {
+		arr = [];
+	};
+	if (d === 0) {
+		x = -b / (2 * a);
+		arr = [x];
+	}
+	if (d > 0) {
+		x = (-b + Math.sqrt(d)) / (2 * a);
+		y = (-b - Math.sqrt(d)) / (2 * a);
+		arr = [y];
+	}
   
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let P = parseInt(percent) / 100 / 12;
+	let S = parseInt(amount) - parseInt(contribution);
+	let n = parseInt(countMonths);
+	let payment = S * (P + (P / (((1 + P) ** n) - 1)));
+	console.log(payment.toFixed(2));
+
 }
