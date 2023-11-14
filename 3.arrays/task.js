ddd8d2d839498e18caff4312f24efa00181b468c
gulyolik <olia.guliaeva@yandex.ru>
2023-11-14 21:54:44 +0300

    Tests adding

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -6,6 +6,12 @@ function compareArrays(arr1, arr2) {
 	}
 }
 
+console.log(compareArrays([8, 9], [6])) // false, разные значения
+console.log(compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5])) // false, разные значения
+console.log(compareArrays([9, 2, 4, 8, 2], [9, 2, 4])) // false, разные значения
+console.log(compareArrays([1, 2, 3], [2, 3, 1])) // false, разные индексы, хотя и одинаковые значения
+console.log(compareArrays([8, 1, 2], [8, 1, 2])) // true
+
 function getUsersNamesInAgeRange(users, gender) {
 	let result = users.filter(user => user.gender === gender).map(users => users.age).reduce((acc, age) => acc + age, 0) 
     / users.filter(people => people.gender === gender).map(ages => ages.age).length;
@@ -13,3 +19,24 @@ function getUsersNamesInAgeRange(users, gender) {
 	return result ? result : 0;
 
 }
+
+const people = [
+    {firstName: "Александр", secondName: "Карпов", age: 17, gender: "мужской"},
+    {firstName: "Егор", secondName: "Морозов", age: 21, gender: "мужской"},
+    {firstName: "Мелисса", secondName: "Леонова", age: 40, gender: "женский"},
+    {firstName: "Мелания", secondName: "Савельева", age: 37, gender: "женский"},
+    {firstName: "Мария", secondName: "Овчинникова", age: 18, gender: "женский"},
+    {firstName: "Марьяна", secondName: "Котова", age: 17, gender: "женский"},
+    {firstName: "Фёдор", secondName: "Селезнев", age: 50, gender: "мужской"},
+    {firstName: "Георгий", secondName: "Петров", age: 35, gender: "мужской"},
+    {firstName: "Даниил", secondName: "Андреев", age: 49, gender: "мужской"},
+    {firstName: "Дарья", secondName: "Савельева", age: 25, gender: "женский"},
+    {firstName: "Михаил", secondName: "Шаров", age: 22, gender: "мужской"},
+    {firstName: "Владислав", secondName: "Давыдов", age: 40, gender: "мужской"},
+    {firstName: "Илья", secondName: "Казаков", age: 35, gender: "мужской"},
+    {firstName: "Евгений", secondName: "Кузьмин", age: 19, gender: "мужской"},
+  ]
+  console.log(getUsersNamesInAgeRange(people, "мужской")); // 32
+  console.log(getUsersNamesInAgeRange(people, "женский")); // 27.4
+  console.log(getUsersNamesInAgeRange([], "женский")); // 0
+  console.log(getUsersNamesInAgeRange(people, "инопланетянин")); // 0
