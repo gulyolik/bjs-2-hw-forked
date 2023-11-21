5ddbf5ec451434edb640397bcba80a5741736569
gulyolik <olia.guliaeva@yandex.ru>
2023-11-21 17:03:34 +0300

    Objects task

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,41 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marks) {
+	if (this.marks) {
+		this.marks = [...this.marks, ...marks];
+	}
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+	return this.marks?.length > 0 ? this.marks.reduce((marksSum, mark, index, marks) => marksSum + mark / marks.length, 0) : 0;
+
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.marks;
+	delete this.subject;
+	this.exclude = reason;
+
 }
+
+let student1 = new Student("Василиса", "женский", 19);
+student1.setSubject("Algebra");
+console.log(student1.getAverage()); // 0
+student1.addMarks(4, 5, 4, 5);
+console.log(student1.getAverage()); // 4.5
+console.log(student1);
+// {age: 19, gender: "женский", marks: [4, 5, 4, 5], name: "Василиса", subject: "Algebra"}
+let student2 = new Student("Артём", "мужской", 25);
+student2.setSubject("Geometry");
+student2.exclude('плохая учёба')
+console.log(student2)
+// {name: "Артём", gender: "мужской", age: 25, excluded: "плохая учёба"}
