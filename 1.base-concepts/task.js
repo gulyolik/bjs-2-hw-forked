32e3401588145cd4a96081e43b4b721f2a0df049
gulyolik <olia.guliaeva@yandex.ru>
2024-01-24 20:21:40 +0300

    Base task fixies

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -14,7 +14,7 @@ function solveEquation(a, b, c) {
 	if (d > 0) {
 		x = (-b + Math.sqrt(d)) / (2 * a);
 		y = (-b - Math.sqrt(d)) / (2 * a);
-		arr = [y];
+		arr = [x, y];
 	}
   
   return arr;
@@ -25,6 +25,7 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 	let S = parseInt(amount) - parseInt(contribution);
 	let n = parseInt(countMonths);
 	let payment = S * (P + (P / (((1 + P) ** n) - 1)));
-	console.log(payment.toFixed(2));
+	let totalMortage = Number((payment* countMonths).toFixed(2));
+	return totalMortage;
 
 }
